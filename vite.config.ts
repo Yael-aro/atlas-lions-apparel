c8fd62900b004afa08022bfcfb285b21da66be5f
Yahya El Arousy <yahyaelarousy@Yahyas-MacBook-Pro.local>
2025-11-26 19:32:51 +0100

    done

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
-  base: '/nom-de-ton-repo/', // ✅ REMPLACE par le nom EXACT de ton repo GitHub
+  base: '/atlas-lions-apparel/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
